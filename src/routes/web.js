b93793ba071ce585455639c4184d31c1bb6df69a
PhatNgo03 <phatngo1908@gmail.com>
2024-08-29 22:41:17 +0700

    MVC

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,18 +1,17 @@
 import express from "express";
-
+import homeController from '../controller/homeController'
 const router = express.Router();
 
 /**
  * 
  * @param {*} app -express app
  */
+
 const initWebRoutes = (app) => {
-    router.get("/", (req, res) => {
-        return res.send("Hello World");
-    })
-
+    //v1 : / path , v2 : handler
+    router.get("/", homeController.handleHelloWorld);
+    router.get("/user", homeController.handleUserPage);
     return app.use("/", router);
-
 }
 
 export default initWebRoutes;
