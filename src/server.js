b6e658994cdba331e920c9f0d532dd7d916a28d5
PhatNgo03 <phatngo1908@gmail.com>
2024-09-06 12:47:27 +0700

    connect api nodejs

--- src/server.js
+++ src/server.js
@@ -1,25 +1,31 @@
 import express from "express";
 import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
+import initApiRoutes from "./routes/api";
+import configCors from "./config/cors";
 import bodyParser from "body-parser";
 // import connection from "./config/connectDB";
 
 require("dotenv").config();
+
 const app = express();
+const PORT = process.env.PORT || 8080;
+
 
 //config view engine
 configViewEngine(app);
 
+//config CORS 
+configCors(app);
 //config body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 //init web routes
 initWebRoutes(app);
+//init api routes
+initApiRoutes(app);
 
-//test connectionDB
-// connection();
-
-const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(">>> JWT Backend is running on the port = " + PORT);
 })
