fd8aca87083e4f1cf4e2e7a04bbbefc6a38c7f0c
PhatNgo03 <phatngo1908@gmail.com>
2024-09-18 20:10:11 +0700

    render FE with bootstrap navbar

--- src/middleware/JWTAction.js
+++ src/middleware/JWTAction.js
@@ -29,12 +29,20 @@ const verifyToken = (token) => {
     return decoded;
 }
 
+const extractToken = (req) => {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        return req.headers.authorization.split(' ')[1];
+    }
+    return null;
+}
 const checkUserJWT = (req, res, next) => {
     if (nonSecurePaths.includes(req.path)) return next();
 
     let cookies = req.cookies;
-    if (cookies && cookies.jwt) {
-        let token = cookies.jwt;
+    let tokenFromHeader = extractToken(req);
+
+    if ((cookies && cookies.jwt) || tokenFromHeader) {
+        let token = cookies && cookies.jwt ? cookies.jwt : tokenFromHeader;
         let decoded = verifyToken(token);
         if (decoded) {
             req.user = decoded;
@@ -89,8 +97,9 @@ const checkUserPermission = (req, res, next) => {
             EM: 'Not authenticated the user',
             DT: ''
         })
+
     }
 }
 module.exports = {
-    createJWT, verifyToken, checkUserJWT, checkUserPermission
+    createJWT, verifyToken, checkUserJWT, checkUserPermission, extractToken
 }
